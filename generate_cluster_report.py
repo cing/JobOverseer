7e8938116ada67ae63f9ef829e3c1458505d6757
Christopher Ing <ing.chris@gmail.com>
2012-03-23 17:59:12 -0400

    Bug fix for not normalizing by number of qstat saves in 24 hours

--- generate_cluster_report.py
+++ generate_cluster_report.py
@@ -14,6 +14,7 @@ if len(argv) < 4:
 
 filename = argv[1]
 previous_days = int(argv[2])
+
 conn = sqlite3.connect(filename)
 c = conn.cursor()
 c.execute("select * from jobs where cluster = '"+argv[3]+"'")
@@ -22,6 +23,13 @@ query_results = c.fetchall()
 #Everything is calculated relative to the current day, but that could change in the future
 nowtime = datetime.combine(date.today(), time())
 
+#This is an important variable to know about.
+#Since we don't know apriori how many times we've collected qstat data
+#in a given 24 hour period, we have to assume that different qstat outputs
+#are separated by at least X number of minutes.
+run_threshold = timedelta(minutes=5)
+
+#cluster use stores the number of cores used for each user on a particular day
 cluster_use = []
 
 #We're going to loop over the number of days in the past (-1 days, -2 days, etc.)
@@ -30,6 +38,8 @@ for prev_day in range(previous_days):
         previous_time_frame = timedelta(days=prev_day)
 
         users_per_day = {}
+        cluster_last_read = 0.
+        cluster_datapoints = 0.
 
         for row in query_results:
             job_datetime = datetime.strptime(row[1], "%Y-%m-%d %X.%f")
@@ -40,8 +50,20 @@ for prev_day in range(previous_days):
                 user_name = str(row[2])
                 if not users_per_day.has_key(user_name):
                     users_per_day[user_name] = int(row[3])*int(row[4])
+                    cluster_lastread = job_datetime
+                    cluster_datapoints = 1
                 else:
                     users_per_day[user_name] += int(row[3])*int(row[4])
+
+                #Only if we have a job sampled more than run_threshold minutes from now 
+                #do we have a new datapoint
+                if (job_datetime - cluster_lastread) > run_threshold:
+                    cluster_datapoints += 1
+                    cluster_lastread = job_datetime
+
+        #normalize the sum of the cores based on how many independent saves there were in 24 hours
+        for user,cores in users_per_day.iteritems():
+            users_per_day[user] /= cluster_datapoints
 
         #Append the users_per_day for a given day to a big list
         cluster_use.append(users_per_day)
